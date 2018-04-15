201f3915a633f86e0fd10da44e42eacdd395851c
SeanBreen <bnyby9@aol.com>
2018-04-15 23:01:41 +0100

    Added Mobile Nav
    Added Marcin's mobile navigation bar to nav bar include

---
+++ Website Files/includes/nav.php
@@ -0,0 +1,12 @@
+<nav>
+ <div class="nav-wrapper blue">
+   <a href="index.php" class="brand-logo">Icarus</a>
+   <ul id="nav-mobile" class="right hide-on-med-and-down">
+     <li><a href="classes.php">Classes</a></li>
+     <li><a href="exams.php">Exams</a></li>
+     <li><a href="students.php">Students</a></li>
+     <li><a href="analytics.php">Analytics</a></li>
+     <li><a href="account.php">Account</a></li>
+   </ul>
+ </div>
+</nav>
