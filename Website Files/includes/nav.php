3a43525fcba6eac62cd8df72e4c366400c6a4b52
SeanBreen <bnyby9@aol.com>
2018-04-15 23:15:58 +0100

    Amended Menu
    Added Marcin's mobile nav bar elements

--- Website Files/includes/nav.php
+++ Website Files/includes/nav.php
@@ -1,12 +1,26 @@
 <nav>
  <div class="nav-wrapper blue">
-   <a href="index.php" class="brand-logo">Icarus</a>
-   <ul id="nav-mobile" class="right hide-on-med-and-down">
-     <li><a href="classes.php">Classes</a></li>
-     <li><a href="exams.php">Exams</a></li>
-     <li><a href="students.php">Students</a></li>
-     <li><a href="analytics.php">Analytics</a></li>
-     <li><a href="account.php">Account</a></li>
-   </ul>
+   <div class="show-on-medium-and-up">
+     <a href="index.php" class="brand-logo">Icarus</a>
+     <ul id="nav-mobile" class="right hide-on-med-and-down">
+       <li><a href="classes.php">Classes</a></li>
+       <li><a href="exams.php">Exams</a></li>
+       <li><a href="students.php">Students</a></li>
+       <li><a href="analytics.php">Analytics</a></li>
+       <li><a href="account.php">Account</a></li>
+     </ul>
+   </div>
+   <div class="hide-on-large blue accent-2">
+     <a href="#" data-activates="slide-out" class=" button-collapse"><i class="white-text">menu</i></a>
+     <ul id="slide-out" class=" side-nav ">
+       <li><a href="index.php"><i class="material-icons">Icarus</i></a></li>
+       <li><a href="classes.php">Classes</a></li>
+       <li><a href="exams.php">Exams</a></li>
+       <li><a href="Students.php">Students</a></li>
+       <li><a href="Analytics.php">Analytics</a></li>
+       <li><a href="Account.php">Account</a></li>
+    </ul>
+  </div>
  </div>
 </nav>
+<script src="js/mobmenu.js"> </script>
